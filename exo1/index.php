67f73f5e6798b6865052b2c838a48612da787fcb
dec <decleggal@gmail.com>
2023-10-06 10:54:38 +0200

    added simple styles

--- exo1/index.php
+++ exo1/index.php
@@ -4,14 +4,18 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>exo1</title>
+        <link rel="stylesheet" href="../styles/styles.css">
     </head>
 
     <body>
-        <?php
-        echo "Hello, World!";
+        <h1>
+            <?php
+            echo "Hello, World!";
 
-        ?>
+            ?>
+        </h1>
+
     </body>
 
 </html>
